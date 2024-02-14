d5360ada23e3f52b7f7d30fd64f8fd8701d28704
sjeuuim <kkm6612@gmail.com>
2024-02-14 17:55:44 +0900

    월드컵 이름 후보 이름 추가
    월드컵 이름 후보 이름 추가

---
+++ src/test/java/com/world/cup/repository/PlayingRepositoryTests.java
@@ -0,0 +1,16 @@
+package com.world.cup.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PlayingRepositoryTests {
+    @Autowired
+    private PlayingRepository playingRepository;
+
+    @Test
+    public void select() {
+
+    }
+}
