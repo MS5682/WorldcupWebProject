6436a3c3401ca95ee473ce8dd51afeb5997ee5d6
Suyeon Lee <tndus28901@gmail.com>
2024-02-14 15:46:48 +0900

    CupApplication.java에 @EnableJpaAuditing

--- src/main/java/com/world/cup/CupApplication.java
+++ src/main/java/com/world/cup/CupApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class CupApplication {
-
+	//@EnableJpaAuditing 추가
 	public static void main(String[] args) {
 		SpringApplication.run(CupApplication.class, args);
 	}
